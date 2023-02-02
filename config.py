936bc06cdc42eaf21d66c9e0e3ed348f2cd62d5c
sabnam777 <115777974+sabnam777@users.noreply.github.com>
2023-02-02 15:02:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,21 @@ import os
 import logging
 
 class Config:
-    
+#Get this value from https://my.telegram.org
     API_ID = int(os.environ.get("API_ID", "1715666"))
+ #Get this value from https://my.telegram.org  
     API_HASH = os.environ.get("API_HASH", "8082d87f5cd157c615d5d2837af21b87")
+ #Your bot token from @BotFather   
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5322382737:AAE9i-QNv5oka-BFn_VrgENJg1BSaPKGvM4") 
+#leave-it
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
+#If you want to add a caption to the forwarded file, enter it here
     CAPTION = os.environ.get("CAPTION", "")
+#Type Of filters (document , audio , photo , video , animation) else type empty to forward almost everything 
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "empty")
-    OWNER_ID = os.environ.get("OWNER_ID", "5143506371")
-    SESSION = os.environ.get("SESSION", "BQDCw-hYoupQiP_AwZBclYZK0O1mpzKW_dSkOI_DKorDFNA6dMbswI6PJdRCKtTpd2jRnxZ48_cTL5qy7anJOtLpBS3HiUKCb9WQEi7A1u_lyjHOJL0fHeN5pycwOePb7tqP6r6qDRjIVAIOZTlS50mIPYpkURqCgFxMekwTXxcNptQn9G2CV61mPux4T-EIssS9TFzZVVkv0OHNOreGgURDtX9ZPj7xBD6cJl1GQGnNOuxsSOpCgK6s9sz5ee14EZp1f-sgzfBpVNCzdaQkRv68b2IGQtPhSJke5Pir59a2tmADhIpaJGUUT6KKWk0UXSoeCYJ4SEbk26KDR7bySgilcdf4KQA")
+#Enter Your Telegram id
+    OWNER_ID = os.environ.get("OWNER_ID", "1105331049")
+#Pyrogram string Seccion - https://replit.com/@KindKobra/Pyrogram-String-Gen?v=1 or any updated pyrogram string session 
+    SESSION = os.environ.get("SESSION", "")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
