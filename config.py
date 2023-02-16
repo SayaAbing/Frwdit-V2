c2fc02bb5917ecdf50ec990e6e6d2748c16b9cf3
sabnam777 <115777974+sabnam777@users.noreply.github.com>
2023-02-16 11:13:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ import logging
 
 class Config:
 #Get this value from https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID", "21879629"))
+    API_ID = int(os.environ.get("API_ID", "1945966"))
  #Get this value from https://my.telegram.org  
-    API_HASH = os.environ.get("API_HASH", "dcb6bfd6d51a8ff5f6aadb01b9fdd11b")
+    API_HASH = os.environ.get("API_HASH", "6b73197f50512e26f5ebd42e73c3315f")
  #Your bot token from @BotFather   
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6135992497:AAHkETqeVK7q7cvkDLL_sWMAXVWrPlVo99U") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5322382737:AAGWqY21L-mw_hPIbUF8sze_HrT9wEdDtGw") 
 #leave-it
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
 #If you want to add a caption to the forwarded file, enter it here
@@ -21,6 +21,6 @@ class Config:
 #Enter Your Telegram id
     OWNER_ID = os.environ.get("OWNER_ID", "1105331049")
 #Pyrogram string Seccion - https://replit.com/@KindKobra/Pyrogram-String-Gen?v=1 or any updated pyrogram string session 
-    SESSION = os.environ.get("SESSION", "BAFN200AByliJ5XijOxLL47FSpctPxIk8EHloEcxYfjgrexr6-SpbfrWqztbd0o6QB_LEocroL6yBxDnIQilaJK32lp7ayQzOe2v0swPdxjTphyW3YNHnGpT5hPi3LTJSRw5ohtfqkq7b1fq1rDgLGMf1j5PlbGCPM5a3Q0Xwc6DMBDPq7YJ0bV9I17w88HyKZOsMCyxpSK1KMgmL7-T1PL7kX9rWuPYKAdKdHUulB_GTaAk6rXK7XsJjQGGlDV68PpMuRfubCSUXGzSCccfGyEcAKimCIPW4Ww5skpLv6mu_y6kRGEaz2hJ49ylw-IIetaLME4dhaCt5ITwzvSoWiJJceclKAAAAAFNQAAoAA")
+    SESSION = os.environ.get("SESSION", "BQAexKqM7vVe5JQ0SWySuFp8Sflw_gjEUD8vZRQSxESkTOIAzVndiiza9CiIEw-jDTKQwLiqlWPG_YQ17mc9wy66kiTnT9P2yA0k1qNIchJWODrLRiCuuFyuYBH01uTVc0ERp0LB_R7YS5-HyvtPKN_5jOkGr7rveq1tJQXrV0aNmee4qxqhTP4zzvDG4aH0F69tQnTSyzf59fBs_xGilWInOMqnPxwl2Y-m2wGeGxzlpzOe-SwkGExU__UBgTnRU_77n4rgCU9q-j2-BCxEvNsTmIDLEFI4IqziPEyrklAMKurgc2OfU2Vugtumg_hCgfOtKABan6kge2qkfo_SpS6ZAAAAAWuYknoA")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
